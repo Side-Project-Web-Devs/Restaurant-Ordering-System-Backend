dbdb0a5e09a2a20462a79e8358b3bb50ebf7c155
Rolly <gilosrolly@sorsogonstatecollege.edu.ph>
2022-08-09 14:19:53 +0800

    feat: create users role and permission

--- qrcode_generator/models.py
+++ qrcode_generator/models.py
@@ -8,15 +8,15 @@ from django.core.files import File
 import random
 class QrCode(models.Model):
    url=models.URLField()
-   image=models.ImageField(upload_to='qrcode',blank=True)
+   image=models.ImageField(upload_to='qrcode', blank=True)
 
-   def save(self,*args,**kwargs):
+   def save(self, *args, **kwargs):
       qrcode_img=qrcode.make(self.url)
-      canvas=Image.new("RGB", (300,300),"white")
+      canvas=Image.new("RGB", (300, 300), "white")
       draw=ImageDraw.Draw(canvas)
       canvas.paste(qrcode_img)
       buffer=BytesIO()
-      canvas.save(buffer,"PNG")
-      self.image.save(f'image{random.randint(0,9999)}',File(buffer),save=False)
+      canvas.save(buffer, "PNG")
+      self.image.save(f'image{random.randint(0,9999)}', File(buffer), save=False)
       canvas.close()
-      super().save(*args,**kwargs)
+      super().save(*args, **kwargs)
